1b2c2eb68e7047cdfd6e7d5adcb466d70cda66ea
Usman <hafizusmanshafiq@gmail.com>
2025-01-15 18:12:38 +0500

    opt. routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # config/routes.rb
 Rails.application.routes.draw do
   root "home#index"
-  get "home/about"
-  get "home/contact"
+  get "about", to: "home#about"
+  get "contact", to: "home#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
